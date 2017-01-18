dd41cac36257e01fda8fc956b1b7343b76f35cdf
haydeng21 <hayden@coilapp.com>
2017-01-17 21:34:19 -0600

    index update

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
 						$servername = "us-cdbr-iron-east-04.cleardb.net";
 						$username = "b4c5e18a9ad6fa";
 						$password = "047e8b9d";
-						$dbname = "heroku_d3e06c073a5cf59";*/
+						$dbname = "heroku_d3e06c073a5cf59";
 						//$con = mysqli_connect('127.0.0.1',"root","","techchat");
 						$con = mysqli_connect($servername,$username,$password,$dbname);
 						if (mysqli_connect_errno()) {
