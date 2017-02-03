bd7fd6a64df2a45d786051b4a0cf51fc97215487
NicolasPerlick <nicolas@coilapp.com>
2017-02-02 22:35:20 -0600

    fixed merge conflict

--- index.php
+++ index.php
@@ -152,6 +152,179 @@
 </head>
 
 <body>
+
+
+
+
+<?php
+	include "mysqli_connection.php";
+	
+	$query = "SELECT COUNT(id) FROM news";
+	$result = $con->query($query);
+	$row = mysqli_fetch_row($result);
+
+	//Here we have the total row count
+	$rows = $row[0];
+	//this is the number of results we want displayed per page
+	$page_rows = 4;
+	//this tells us the page number of our last page
+	$last = ceil($rows/$page_rows);
+	//this makes sure $last can't be less than 1
+	if ($last < 1) {
+		$last = 1;
+	}
+	// establish the $pagenum variable
+	$pagenum = 1;
+	$dummy = $_GET['pn'];
+
+	//get pagenum from URL vars if it is present, else it is = 1
+	if (isset($_GET['pn'])) {
+		$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
+	}
+	// this makes sure the page number isn't below 1, or more than our $last page
+	if ($pagenum < 1) {
+		$pagenum = 1;
+	}
+	else if ($pagenum > $last) {
+		$pagenum = $last;
+	}
+	// sets the range of rows to query for the chosen $pagenum
+	$limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' . $page_rows;
+	//query just 1 page worth of rows by applying $limit
+	$query = "SELECT id, headline, media, time_stamp, tag FROM articles ORDER BY id DESC $limit";
+	$result = $con->query($query);
+	//shows the user what page they are on and the total number of pages
+	$textline1 = "Dummy (<b>$rows</b>)";
+	$textline2 = "Page <b>$pagenum</b> of <b>$last</b>";
+	//establish $pagination on Crtls variable
+	$paginationCtrls = '';
+	//if there is more then 1 page worth of results
+	if ($last != 1) {
+		// first we check if we are on page one. If we are then we don't need a link to the previous page or the first page so we do nothing. If we aren't then we generate links to the first page, and to the previous page.
+		if ($pagenum > 1) {
+			$previous = $pagenum - 1;
+			$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'">Previous</a>';
+			//render clickable number links that should appear on the left of the target page number
+			for ($i = $pagenum-4; $i<$pagenum; $i++) {
+				if ($i > 0) {
+					$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a> ';
+				}
+			}
+
+		}
+
+		// render the target page number without it being a link. Dead link.
+		$paginationCtrls .= ''.$pagenum;
+		//render clickable number links that would appear on the right
+		for ($i = $pagenum+1; $i <=$last; $i++) {
+			$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a>';
+			if ($i >= $pagenum+4) {
+				break;
+			}
+		}
+		// shows the word next
+		if ($pagenum != $last) {
+			$next = $pagenum + 1;
+			$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'">Next</a> ';
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	    
+	<title>TechChat News</title>
+	    
+	<!-- Custom CSS -->
+	<!--<link href="style.php" rel="stylesheet" title="index style" type="text/css" media="all" />-->
+	<link href="logo-nav.css" rel="stylesheet" title="basic style" type="text/css" media="all" />
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+	<!-- Bootstrap Core CSS -->
+	<link href="bootstrap.min.css" rel="stylesheet" title="basic style" type="text/css" media="all"/>
+
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href= "style.php"/>
+	    
+	<!--[if lt IE 9]>
+	        
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	        
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    
+	<![endif]-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script>
+    $(function(){
+
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
+
+
+	function SetCookie(c_name,value,expiredays)
+	{
+	    var exdate=new Date()
+	    exdate.setDate(exdate.getDate()+expiredays)
+	    document.cookie=c_name+ "=" +escape(value)+
+	    ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+	}
+
+
+	</script>
+
+</head>
+
+<body>
+<!--Facebook API integration-->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '224533154659650',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -163,24 +336,6 @@
 
 	</script>
 
-	<!--Facebook API integration-->
-	<script>
-	  window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '224533154659650',
-	      xfbml      : true,
-	      version    : 'v2.8'
-	    });
-	  };
-
-	  (function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-	</script>
 
 	<!--Connect to facebook -->
 	<div id="fb-root"></div>
@@ -231,7 +386,7 @@
 						
 						include "mysqli_connection.php";
 
-						$query = "SELECT vidID FROM jumbotron ORDER BY id ASC";
+						$query = "SELECT vidID FROM videoday ORDER BY id DESC";
 							//query the result and assign in to $result
 							$result = $con->query($query);
 							//if the row is not empty
@@ -262,80 +417,82 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8">
+						<h1 id="stories"> Short Stories </h1>
 						<?php
 
 						include "mysqli_connection.php";
 
-						$query = "SELECT id, headline, media,time_stamp FROM news ORDER BY id DESC $limit";
+						$query = "SELECT id, headline, media, time_stamp, tag FROM articles ORDER BY id DESC $limit";
 						//query the result and assign in to $result
 						$result = $con->query($query);
 						//if the row is not empty
 						if ($result->num_rows > 0) {
 							while ($fetch=$result->fetch_assoc()) {
-								//click on article and it takes you to id in database. Convert image to binary code you can use
-								echo '<a href="articles.php?image=' . $fetch['id'] .'"><img src="data:image/jpeg;base64,'.base64_encode( $fetch['media'] ).'" class="imgHover" /></a>';
-								//new line
-								echo nl2br("\n");
-								echo $fetch['headline'];	
-								echo nl2br("\n");
-								echo nl2br("\n");
-								echo $fetch['time_stamp'];
-								echo nl2br("\n");
-
-
+								echo '<div class="col-md-6">';
+									//click on article and it takes you to id in database. Convert image to binary code you can use
+									/* echo '<h5 class="pull-left">'. $fetch["time_stamp"]. '</h5>'.'<h5 class="pull-right">'. $fetch["tag"]. '</h5>'; */
+									echo '<a href="articles.php?image=' . $fetch['id'] .'"><img src="data:image/jpeg;base64,'.base64_encode( $fetch['media'] ).'" class="imgHover img-responsive center-block" /></a>';
+									//new line
+									echo nl2br("\n");
+									echo '<div class = "headline">';
+										echo $fetch['headline'];
+									echo '</div>';	
+									echo nl2br("\n");
+								echo '</div>';
 							}
 						}
 						
+						
 						//close the connection
 						$con->close();
-						?>
-						
-
-						
-			    		
+						?>	
 					</div>
+				
+				
 
-					<div class="fix">
-						<div class="col-md-4">
-							<!--<?php
+				
+				<div class="col-md-4">
+					<h1 id="videos"> Videos </h1>
+					<?php
 
-							$query = "SELECT vidID FROM videos ORDER BY id ASC";
-							//query the result and assign in to $result
-							$result = $con->query($query);
-							//if the row is not empty
+					include "mysqli_connection.php";
 
-							if ($result->num_rows > 0) {
-								while ($fetch=$result->fetch_assoc()) {
-									//convert array to string
-									$string_version = implode(',', $fetch);
-								}
-								
-							}
+					$query = "SELECT vidID, headline FROM videos ORDER BY id DESC";
+					//query the result and assign in to $result
+					$result = $con->query($query);
+					//if the row is not empty
 
-							//close the connection
-							$con->close();
-							?>
+					if ($result->num_rows > 0) {
+						while ($fetch=$result->fetch_assoc()) {
+							//convert array to string
+							$string_version = implode(',', $fetch);
+							echo '<iframe id="videoList" width="300" height="300" src="https://www.youtube.com/embed/<?php echo $string_version; ?>?rel=0&showinfo=0&autohide=1&autoplay=0" frameborder="0" allowfullscreen volume="0"></iframe>';
+							echo nl2br("\n");
+							echo '<div class = "headline">';
+								echo $fetch['headline'];
+							echo '</div>';	
+							echo nl2br("\n");
+						}
+						
+					}
 
+					//close the connection
+					$con->close();
+					?>
+					
 
-							<iframe id="videoDay" width="400" height="200" src="https://www.youtube.com/embed/<?php echo $string_version; ?>?rel=0&showinfo=0&autohide=1&autoplay=0" frameborder="0" allowfullscreen volume="0"></iframe>
-							<div
-								class="fb-like"
-								data-share="true"
-								data-width="450"
-								data-show-faces="true">
-							</div>
+					
+					
 
-							-->
-							
-							
-						</div>
-					</div>t
+					
+					
 				</div>
+				</div>
+				
 			</div>
 			<div>
 			    <div id = "pagination_controls"><?php echo $paginationCtrls; ?></div>
-
-			 </div>
+			</div>
 				<div class="footer">
 					<div class="row">
 						<div class="col-lg-12">
